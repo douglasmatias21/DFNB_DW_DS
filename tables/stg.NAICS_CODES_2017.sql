a3a3940a695a6157108fbcf080b02b3bb098d432
douglasmatias21 <71949172+douglasmatias21@users.noreply.github.com>
2021-03-28 11:03:01 -0600

    IT 243 - Added LoadNAICSCodeHierDim_DS.dtsx

--- tables/stg.NAICS_CODES_2017.sql
+++ tables/stg.NAICS_CODES_2017.sql
@@ -5,7 +5,7 @@ PURPOSE: Create the stg.NAICS_CODES_2017 table
 MODIFICATION LOG:
 Ver      Date        Author        Description
 -----   ----------   -----------   -------------------------------------------------------------------------------
-1.0     03/05/2021   KMATZEN       1. Built this table for LDS BC IT240
+1.0     03/15/2021   DSILVA       1. Built this table for LDS BC IT240
 
 
 
@@ -26,12 +26,12 @@ distributed under the same license terms.
 USE [DFNB3]
 GO
 
-/****** Object:  Table [stg].[NAICS_CODES_2017]    Script Date: 3/5/2021 8:59:45 PM ******/
+/****** Object:  Table [stg].[NAICS_CODES_2017]    Script Date: 3/15/2021 8:59:45 PM ******/
 IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[stg].[NAICS_CODES_2017]') AND type in (N'U'))
 DROP TABLE [stg].[NAICS_CODES_2017]
 GO
 
-/****** Object:  Table [stg].[NAICS_CODES_2017]    Script Date: 3/5/2021 8:59:45 PM ******/
+/****** Object:  Table [stg].[NAICS_CODES_2017]    Script Date: 3/15/2021 8:59:45 PM ******/
 SET ANSI_NULLS ON
 GO
 
